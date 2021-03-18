79b227e542e8bcf3b665d31e7180f4cbdb196751
Vidhi Khaitan <vidhik2002@gmail.com>
2021-03-18 14:08:03 +0530

    feat:testing route

--- logout.js
+++ logout.js
@@ -4,6 +4,8 @@ const router = express.Router();
 router.post('/', (req, res) => {
     res.json({ accessToken: null })
 });
-
+router.get('/', (req,res) =>{
+    res.json({msg: "hi"})
+})
 
 module.exports = router;
