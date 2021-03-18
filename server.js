adfa5000ac21d22cb0369a71de3c1c11f7ce71b8
Vidhi Khaitan <vidhik2002@gmail.com>
2021-03-18 14:16:37 +0530

    fix: env port variable

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use('/login', loginRoute);
 app.use('/logout', logoutRoute);
 app.use('/score', scoreRoute);
 
+const port = process.env.PORT || 3000
 
 
-
-app.listen(3000,() =>{
-    console.log("server started")
-})
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
